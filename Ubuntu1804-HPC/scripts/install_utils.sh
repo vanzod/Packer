32ba35dda7fdcb00755b9a9b5af4efa2c148fa74
Davide Vanzo <davide.vanzo@microsoft.com>
2021-03-19 16:01:49 -0500

    Removed unnecessary packages

--- Ubuntu1804-HPC/scripts/install_utils.sh
+++ Ubuntu1804-HPC/scripts/install_utils.sh
@@ -2,29 +2,6 @@
 set -ex
 
 apt-get update
-apt-get -y install build-essential
-apt-get -y install numactl \
-                   rpm \
-                   libnuma-dev \
-                   libmpc-dev \
-                   libmpfr-dev \
-                   libxml2-dev \
-                   m4 \
-                   byacc \
-                   python-dev \
-                   python-setuptools \
-                   tcl \
-                   environment-modules \
-                   tk \
-                   texinfo \
-                   libudev-dev \
-                   binutils \
-                   binutils-dev \
-                   selinux-policy-dev \
-                   flex \
-                   libnl-3-dev \
-                   libnl-route-3-dev \
-                   libnl-3-200 \
-                   bison \
-                   libnl-route-3-200 \
-                   gfortran
+apt-get -y install build-essential \
+                   numactl \
+                   libnuma-dev
